8cacebe0d7fa7558806a4c6b07f6422b7560623a
Lucky44457 <maribelwate@gmail.com>
2025-08-20 15:26:56 +0530

    Create seever.py

---
+++ seever.py
@@ -0,0 +1,18 @@
+from flask import Flask
+import threading
+import os
+import main  # your bot file
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Bot is running!"
+
+def run_bot():
+    os.system("python3 main.py")
+
+if __name__ == "__main__":
+    threading.Thread(target=run_bot).start()
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 10000)))
+  
