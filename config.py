92fdf847d4f27328cc70b544c1b5ab38906c621e
Lucky44457 <maribelwate@gmail.com>
2025-08-25 14:58:46 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 
 API_ID = int(os.environ.get("API_ID", "29736812"))
 API_HASH = os.environ.get("API_HASH", "d22c2fc9b388eef56e456f546b1d9c1a")
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "8010956393:AAHlfDEJFKjC3RRTQ7kSxHE53AB4-w-h3Kw")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
 
 # Use a list for AUTH_USERS instead of a set
 ADMINS = [6197171929, 5728458734, 1838614580 ]  # Replace with your actual admin user IDs
